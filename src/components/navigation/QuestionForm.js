f03fbeb0a7f591e035836adf54f7b136c020848b
Dariusz <darglk@gmail.com>
2018-09-02 10:42:12 +0200

    added styling for quiz form

--- src/components/navigation/QuestionForm.js
+++ src/components/navigation/QuestionForm.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react';
-import { FormGroup, Form, Label, Input, Button } from 'reactstrap';
+import { 
+    FormGroup, 
+    Form, 
+    Input, 
+    Button, 
+    ListGroup, 
+    ListGroupItem, 
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup
+ } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 class QuestionForm extends Component {
     render() {
         const answers = this.props.question.answers;
-        const inputs = Object.keys(answers).map(key => {
+        const inputs = Object.keys(answers).map((key, index) => {
             return (
-                <FormGroup key={key}>
-                    <Label check>
-                        <Input 
-                        type="radio" 
-                        name="question" 
-                        value={key} 
-                        onChange={this.props.handleChange}/>{' '}
-                        {key}: {answers[key]}
-                    </Label>
-                </FormGroup>
+                <ListGroupItem key={key}>
+                    <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <Input
+                                    checked={index === 0 ? "checked": ""}
+                                    addon
+                                    type="radio" 
+                                    name="question" 
+                                    value={key} 
+                                    onChange={this.props.handleChange}
+                                    aria-label="Radio for following text input"
+                                    />
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder={key+ ": "+ answers[key]} disabled/>
+                    </InputGroup>
+                </ListGroupItem>
             );
         });
         return (
-            <Form onSubmit={this.props.handleSubmit}>
-                <h6>Now, {this.props.answering.name} is answering the question.</h6>
-                <FormGroup tag="fieldset">
-                    <legend>{this.props.question.question}</legend>
-                    {inputs}
-                </FormGroup>
-                <Button type="submit">Submit</Button>
-            </Form>
+            <ListGroup>
+                <Form onSubmit={this.props.handleSubmit}>
+                    <h4>Now, <strong style={{color: "green"}}>{this.props.answering.name}</strong> is answering the question.</h4>
+                    <FormGroup tag="fieldset">
+                        <ListGroupItem active>{this.props.question.question}</ListGroupItem>
+                        {inputs}
+                    </FormGroup>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </ListGroup>
         );
     }
 }
